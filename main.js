5cf171b682c560e9eaef75fbe370d41449c7bc0c
demiladebamgbose <demiladebamgbose94@gmail.com>
2016-09-30 12:17:35 +0100

    change interface

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile('__dirname' + '/public/index.html');
-  });
+});
 
 app.listen(8080, function () {
   console.log('Server started on port 8080');
